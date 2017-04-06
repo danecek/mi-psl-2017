6d77c51a2a2bef555b1244f66e11099f0d6529f6
danecek <danecek@fit.cvut.cz>
2017-04-06 12:33:29 +0200

    zzz

--- CombinatorsHelloWorld/src/main/scala/Main.scala
+++ CombinatorsHelloWorld/src/main/scala/Main.scala
@@ -1,7 +1,15 @@
 import scala.util.parsing.combinator.JavaTokenParsers
+
 object Main extends JavaTokenParsers {
-  def main(args: Array[String]): Unit = {
-    println(parseAll(wholeNumber ^^ {_.toInt }, "3").get + 3)
+  def main(args: Array[String]) {
+
+    println(parse(ident ~ ident, "xxx yyy"))
+    println(parse("xxx" ~ "yyy", "xxxyyy"))
+
+
+    println(parseAll(wholeNumber ^^ {
+      _.toInt
+    }, "3").get + 3)
 
   }
 }
